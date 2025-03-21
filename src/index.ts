ae8ef8b70b58636c3e7e584bc8ebcbeae9b6caba
Ygor <ygor.araujo@dataprev.gov.br>
2025-03-21 18:29:56 -0300

    removing https

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,29 @@
-import { Webhook, WebhookSender } from "./httpClient/webhookSender"
-import { readJsonFile } from "./scripts/jsonReader"
+import { Webhook, WebhookSender } from "./httpClient/webhookSender";
+import { readJsonFile } from "./scripts/jsonReader";
 
 let creatorsfyHost = `localhost`;
 const creatorsfyPort = 8181;
 
 if (creatorsfyPort != null)
-    creatorsfyHost = `${creatorsfyHost}:${creatorsfyPort}`
+  creatorsfyHost = `${creatorsfyHost}:${creatorsfyPort}`;
 
 async function init() {
-    const url = new URL(`/webhook/order`, `https://${creatorsfyHost}`)
-    const orders = readJsonFile()
+  const url = new URL(`/webhook/order`, `http://${creatorsfyHost}`);
+  const orders = readJsonFile();
 
-    let ordersCount = 0;
-    while (ordersCount < orders.length) {
-        const webhook = new Webhook(url, orders[ordersCount])
-        const webhookSender = new WebhookSender()
+  let ordersCount = 0;
+  while (ordersCount < orders.length) {
+    const webhook = new Webhook(url, orders[ordersCount]);
+    const webhookSender = new WebhookSender();
 
-        try {
-            const response = await webhookSender.sendWebhook(webhook)
-            console.log(`Webhook sent: ${response.status}`)
-            ordersCount++
-        } catch (error) {
-            console.error(`Error sending webhook: ${error}`)
-        }
-
+    try {
+      const response = await webhookSender.sendWebhook(webhook);
+      console.log(`Webhook sent: ${response.status}`);
+      ordersCount++;
+    } catch (error) {
+      console.error(`Error sending webhook: ${error}`);
     }
+  }
 }
 
-init()
+init();
